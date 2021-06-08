c02e12cac69488f869a1837d60b2c33f797aea84
Rahulm0106 <59145095+Rahulm0106@users.noreply.github.com>
2021-06-08 14:47:03 +0530

    new

--- Portfolio/main/views.py
+++ Portfolio/main/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
+from django.shortcuts import render
+from django.conf import settings
 
 # Create your views here.
 
 def index(request):
-    context={}
+    context={
+        "name": settings.DATA["NAME"],
+        "about_me": settings.DATA["ABOUT_ME"],
+    }
     return render(request, 'main/index.html', context)
 
 def projects(request):
-    context={}
+    context={
+        "projects": settings.DATA["PROJECTS"]
+    }
     return render(request, 'main/projects.html', context)
 
 def languages(request):
-    context={}
+    context={
+        "languages": settings.DATA["LANGUAGES"]
+    }
     return render(request, 'main/languages.html', context)
